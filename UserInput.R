e0794dd0d5c309dbb339acbabfbf629dbcbb53c4
ketki-analytics <ketki.pandey@flipkart.com>
2016-05-25 12:54:59 +0530

    Update UserInput.R

--- UserInput.R
+++ UserInput.R
@@ -1,20 +1,20 @@
 ####################################################################################################
 ## Setting up the Working Directory
-setwd('C:/Users/somsubhra.g/Google Drive/eclipse/routeOptimization_R')
+setwd('C:\Users\ketki.pandey\Documents\GitHub\Trial')
 ## Data File - Contains Address,Pincode,City,State, LBH and Slot Details
-fileName = "BHI_05_04_2016.xlsx"
+fileName = "optimized bangalore geo codes omr.xlsx"
 ## GitHub Directory
-githubDir = "https://raw.githubusercontent.com/somsubhra88/Vehicle_Route_Optimization/MultipleCuts/"
+githubDir = "https://raw.githubusercontent.com/ketki-analytics/trial/master/"
 ####################################################################################################
 ## Hub Lat Lng
 # hubLatLng = c(77.6533668, 12.8852659) #Kudlu
 # hubLatLng = c(77.315002, 28.714921, ) #Mandoli
 # hubLatLng = c(88.317647, 22.739977 )  #Kolkata
-hubLatLng = c(73.034974, 19.237481)   #Bhiwandi
+hubLatLng = c(12.995565,77.666814)   #OMR
 ####################################################################################################
 # Shift start timing for the Vans
 # shiftStart = c(750,1350) # For Kolkata, Kudlu
-shiftStart = c(900,1300) # for Bhiwandi
+shiftStart = c(900,1400) # for Bhiwandi
 # Average Speed for different Location
 avgSpeed = 15 # For Kudlu, Bhiwandi, Mandoli
 # avgSpeed = 20 # For Kolkata Only
